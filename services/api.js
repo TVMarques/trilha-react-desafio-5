4cb52b2c47a686e52ea26af8164306e1af3d4ee3
Thiago Marques <thiago77mar@gmail.com>
2026-03-15 14:09:10 -0300

    Colocando a busca de um post específico

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://fcskdwfjeadvfufqlygk.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjc2tkd2ZqZWFkdmZ1ZnFseWdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwOTE5NTUsImV4cCI6MjA4ODY2Nzk1NX0.iL3VgdKd-TtNRu5RM8nuCZ0xPo5i-W3wHD4-fArrxjY",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjc2tkd2ZqZWFkdmZ1ZnFseWdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwOTE5NTUsImV4cCI6MjA4ODY2Nzk1NX0.iL3VgdKd-TtNRu5RM8nuCZ0xPo5i-W3wHD4-fArrxjY"
     }
 })
